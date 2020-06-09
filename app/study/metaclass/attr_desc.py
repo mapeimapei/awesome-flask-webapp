e327332ebd2fa600468791e62f3d5bb74eeb42f5
9725029@qq.com <9725029@qq.com>
2020-06-09 21:42:56 +0800

    元类编程2

--- app/study/metaclass/attr_desc.py
+++ app/study/metaclass/attr_desc.py
@@ -3,6 +3,26 @@ import numbers
 
 
 # 属性描述符和属性查找过程
+'''
+如果user是某个类的实例，那么user.age(以及等价的getatter(user,"age"))
+首先调用__getattribute__。如果类定义了__getattr__方法，
+那么在__getattribute__抛出AttributeError的时候就会调用到__getattr__,
+而对于描述符(__get__)的调用，则是发生在__getattribute__内部的。
+user = User(),那么user.age顺序如下：
+1，如果“age”是出现在User或者基类的__dict__中，且age是data descriptor，那么调用其__get__方法，否则
+2，如果“age”出现在obj（user）的__dict__中，那么直接返回obj.__dict__['age'],否则
+3，如果“age”出现在User或者其基类的__dict__中
+3.1 如果age是non-data descriptor,那么调用其__get__方法，否则
+3.2 返回__dict__['age']
+4 如果User有__getattr__ 方法，调用__getattr__方法，否则
+5 抛出AttributeError
+'''
+
+
+
+
+
+
 
 class IntField:
     #数据描述符
@@ -28,32 +48,14 @@ class NonDataIntField:
 
 class User:
     age = IntField()
-
-
-#
-# class User:
-#     def __init__(self, name, email,birthday):
-#         self.name = name
-#         self.email = email
-#         self.birthday = birthday
-#         self._age = 0 # 不想对外暴露
-#
-#     # def get_age(self):
-#     #     return datetime.now().year - self.birthday.year
-#
-#     @property
-#     def age(self):
-#         return datetime.now().year - self.birthday.year
-#
-#     @age.setter
-#     def age(self,value):
-#         self._age = value
-
+    #age = NonDataIntField()
 
 if __name__ == "__main__":
     user = User()
     user.age = 30
-    user.age = -1
+    #user.age = -1
+    user.__dict__["age"] = "abc"
+    print(user.__dict__)
     print(user.age)
     # user = User("bobby", date(year=1987, month=1, day=1))
     # user.age = 30
---
+++ app/study/metaclass/new_init.py
@@ -0,0 +1,18 @@
+
+
+class User:
+    def __new__(cls,*args,**kwargs):
+        print("in new")
+        return super().__new__(cls)
+
+    def __init__(self,name):
+        print("in init")
+        self.name = name
+
+#new 是用来控制对象的生成过程，在对象生成之前
+#init 是用来完善对象的
+#如果new方法不返回对象，则不会调用init函数
+if __name__ == "__main__":
+
+    user = User(name="bobby")
+
